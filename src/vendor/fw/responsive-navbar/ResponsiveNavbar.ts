43b9634ad9e4a19bb883153e3a7d99421306dd06
Ioullian Alexeev <ioulian@wax.be>
2019-04-30 16:47:26 +0200

    optimized documentation and fixed TODO in responsive navbar

--- src/vendor/fw/responsive-navbar/ResponsiveNavbar.ts
+++ src/vendor/fw/responsive-navbar/ResponsiveNavbar.ts
@@ -27,10 +27,14 @@ export class ResponsiveNavbar {
       if (e.target instanceof HTMLElement && e.target.closest('.has-dropdown') !== null) {
         const elContainer = e.target.closest('.has-dropdown')
 
-        // TODO: check if item has an url, and if not, the submenu can be closed then?
         if (elContainer.classList.contains('is-open') === false) {
           e.preventDefault()
           elContainer.classList.add('is-open')
+        } else if (e.target.parentNode === elContainer && e.target.getAttribute('href') === null) {
+          // If the item is not clickable and is direct child of parent then close the dropdown
+          // This can happen if the menu item itself is not clickable, only the children are
+          e.preventDefault()
+          elContainer.classList.remove('is-open')
         }
       }
     })
