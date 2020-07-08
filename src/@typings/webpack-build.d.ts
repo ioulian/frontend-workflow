4b96dc81e8c9978574bbe58a0886ee58f2db6758
Ioulian Alexeev <1311270+ioulian@users.noreply.github.com>
2020-07-08 15:08:45 +0200

    Update webpack-build.d.ts

--- src/@typings/webpack-build.d.ts
+++ src/@typings/webpack-build.d.ts
@@ -2,3 +2,4 @@ declare const __SERVICE_WORKER_ACTIVE__: boolean
 declare const __IS_DEV__: boolean
 declare const __IS_PROD__: boolean
 declare const __LANG__: boolean
+declare const __PUBLIC_PATH__: string
